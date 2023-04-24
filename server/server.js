16c84c3cb6383c42ec46d60be866c911aa6b88ca
Michael Gaynor <mgaynor228@gmail.com>
2023-04-24 17:21:21 -0500

    deploying app

--- server/server.js
+++ server/server.js
@@ -36,8 +36,8 @@ app.use((err,req,res,next) =>{
 // Deploying app
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join( "client", "build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 
