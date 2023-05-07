6b64bd0dc33da9fec0f5605e57b714970b55c1a4
Michael Gaynor <mgaynor228@gmail.com>
2023-05-07 14:54:31 -0500

    deploying

--- server.js
+++ server.js
@@ -22,26 +22,12 @@ app.use(cors({
 // .then(()=> console.log("Connected to MongoDB"))
 // .catch(err => console.error(err));
 
-// Connect to MongoDB database
-// mongoose.set('strictQuery', false);
-// mongoose.connect(process.env.MONGODB_URI, () =>
-//   console.log('Connected to the DB')
-// );
+//Connect to MongoDB database
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log('Connected to database');
+mongoose.connect(process.env.MONGODB_URI, () =>
+  console.log('Connected to the DB')
+);
 
-        app.listen(3001, (err) => {
-            if (err) {
-                throw new Error(err);
-            }
-            console.log('Server is Successfully Running, and App is listening on port 3001 ');
-        });
-    })
-    .catch(err => {
-        console.error(err);
-    });
 
 //Routes
 app.use("/favoritesData", require("./routes/favoriteData.jsx"))
@@ -59,6 +45,6 @@ app.get("*", (req, res) => {
 });
 
 
-// app.listen(3001, () => {
-//     console.log("Server Is Running")
-// })
+app.listen(3001, () => {
+    console.log("Server Is Running")
+})
